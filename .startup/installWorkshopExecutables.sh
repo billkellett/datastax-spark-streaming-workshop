edf32f32dbe43c144cc433c350d7764d1a990260
billkellett <44028163+billkellett@users.noreply.github.com>
2019-06-30 14:50:55 -0400

    Update installWorkshopExecutables.sh

--- .startup/installWorkshopExecutables.sh
+++ .startup/installWorkshopExecutables.sh
@@ -9,7 +9,7 @@ echo "Starting Workshop Executables installation..."
 if [ `hostname` == 'node0' ]
 then
   echo "Downloading Event Generator..."
-  curl -o data/kafka-event-generator-1.0-SNAPSHOT-jar-with-dependencies.jar 'http://billkellett.net/dse/kafka-event-generator-1.0-SNAPSHOT-jar-with-dependencies.jar' -L   
+  curl -o executables/kafka-event-generator-1.0-SNAPSHOT-jar-with-dependencies.jar 'http://billkellett.net/dse/kafka-event-generator-1.0-SNAPSHOT-jar-with-dependencies.jar' -L   
   echo "... Event Generator download complete."
 fi
-echo "... Finished Kafka Installation."
+echo "... Finished Workshop Executables Installation."
