2d9d8ad7a9ed9d39287af71132d2384f82a5599a
billkellett <44028163+billkellett@users.noreply.github.com>
2019-07-08 09:38:18 -0400

    Update installWorkshopExecutables.sh

--- .startup/installWorkshopExecutables.sh
+++ .startup/installWorkshopExecutables.sh
@@ -11,5 +11,14 @@ then
   echo "Downloading Event Generator..."
   curl -o executables/kafka-event-generator-1.0-SNAPSHOT-jar-with-dependencies.jar 'http://billkellett.net/dse/kafka-event-generator-1.0-SNAPSHOT-jar-with-dependencies.jar' -L   
   echo "... Event Generator download complete."
+  
+  echo "Downloading Standalone Event Consumer..."
+  curl -o executables/datastax-spark-streaming-jar-with-dependencies.jar 'http://billkellett.net/dse/datastax-spark-streaming-jar-with-dependencies.jar' -L   
+  echo "... Standalone Event Consumer download complete."
+  
+  echo "Downloading DSE Event Consumer..."
+  curl -o executables/datastax-spark-streaming-dse-jar-with-dependencies.jar 'http://billkellett.net/dse/datastax-spark-streaming-dse-jar-with-dependencies.jar' -L   
+  echo "... DSE Event Consumer download complete."
+  
 fi
 echo "... Finished Workshop Executables Installation."
